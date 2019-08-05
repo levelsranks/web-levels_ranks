68b669608717ca9246fa4746584ae8df02fc70ec
Anastasia S <33180989+M0st1ce@users.noreply.github.com>
2019-08-05 18:15:34 +0300

    fix

--- web_dev/app/page/general/sidebar.php
+++ web_dev/app/page/general/sidebar.php
@@ -12,13 +12,14 @@
 if ( ! empty( $_SESSION['steamid'] ) ) {
     // С помощью цикла делаем запросы к базе данных.
     for ( $d = 0; $d < $Db->table_count['LevelsRanks']; $d++ ) {
-        $res_data_sidebar[] = ['name_servers' => $Db->db_data['LevelsRanks'][$d]['name'],
-                               'mod' => $Db->db_data['LevelsRanks'][$d]['mod'],
-                               'ranks_pack' => $Db->db_data['LevelsRanks'][$d]['ranks_pack'],
-                               'data_servers' => $Db->db_data['LevelsRanks'][$d]['Table']];
+        $res_data_sidebar[$d] = ['name_servers' => $Db->db_data['LevelsRanks'][$d]['name'],
+            'mod' => $Db->db_data['LevelsRanks'][$d]['mod'],
+            'ranks_pack' => $Db->db_data['LevelsRanks'][$d]['ranks_pack'],
+            'data_servers' => $Db->db_data['LevelsRanks'][$d]['Table']];
         $base_info = $Db->query('LevelsRanks', $Db->db_data['LevelsRanks'][$d]['USER_ID'], $Db->db_data['LevelsRanks'][$d]['DB_num'], 'SELECT name, lastconnect, rank FROM ' . $Db->db_data['LevelsRanks'][$d]["Table"] . ' where steam="' . $_SESSION['steamid32'] . '"' );
         if ($base_info != '') {
             $user_auth[] = $base_info;
+            $server_info[] = $res_data_sidebar[$d];
             $lastconnect[] = $base_info['lastconnect'];
         }
     }
@@ -26,29 +27,26 @@ if ( ! empty( $_SESSION['steamid'] ) ) {
     if ( $user_auth[0] == '' || $user_auth[0] == '' ) {
         $user_auth[0] = ['name' => 'Неизвестно', 'lastconnect' => '', 'rank' => '00'];
         $lastconnect = '-';
-        $res_data_sidebar[0]['name_servers'] = 'Неизвестно';
+        $server_info[0]['name_servers'] = 'Неизвестно';
     }
 
     $user_rank_count = sizeof( $user_auth );
-    for ( $d = 0, $c = sizeof( $Modules->arr_module_init['sidebar'] ); $d < $c; $d++ ):
-        for ( $_d = 0, $_c = sizeof( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'] ); $_d < $_c; $_d++ ):
-         $sidebar_inc[ $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['sidebar_directory'] ][] = $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ];
-        endfor;
-    endfor;
-}?>
+
+}
+?>
 <aside class="main-sidebar offcanvas">
     <section class="sidebar">
         <div class="user-sidebar-block">
             <div class="user-info">
-                <a href="<?php empty( $_SESSION['steamid32'] ) ? print '#' : print $General->arr_general['site'] . '?page=profiles&profile=' . $_SESSION['steamid32']?>">
+                <a href="<?php empty( $_SESSION['steamid32'] ) ? print '#' : print $General->arr_general['site'] . '?page=profiles&profile=' . $_SESSION['steamid32'] . '&search=1'?>">
                     <?php ! empty( $_SESSION['steamid32'] ) && $General->get_js_relevance_avatar( $_SESSION['steamid32'] )?>
                     <img id="<?php empty( $_SESSION['steamid'] ) ? print 0 : print $_SESSION['steamid']?>" ondrag="return false" ondragstart="return false" src="
                     <?php if( empty( $_SESSION['steamid'] ) ) { echo 'storage/cache/img/avatars_random/' . rand(1,30) . '_xs.jpg';
                     } else{
-                    if ( $General->arr_general['avatars'] == 1){ echo $General->getAvatar($_SESSION['steamid'],1);
-                    } elseif( $General->arr_general['avatars'] == 2 || $General->arr_general['avatars'] == 0) {
-                        echo 'storage/cache/img/avatars_random/' . rand(1,30) . '_xs.jpg';
-                    }}?>"></a>
+                        if ( $General->arr_general['avatars'] == 1){ echo $General->getAvatar($_SESSION['steamid'],1);
+                        } elseif( $General->arr_general['avatars'] == 2 || $General->arr_general['avatars'] == 0) {
+                            echo 'storage/cache/img/avatars_random/' . rand(1,30) . '_xs.jpg';
+                        }}?>"></a>
                 <div class="user-details">
                     <span class="user_name"><?php empty( $_SESSION['steamid'] ) ? print 'Герой без имени' : print $user_auth[0]['name']?></span>
                     <?php if( ! empty( $_SESSION['steamid'] ) ):?>
@@ -61,24 +59,24 @@ if ( ! empty( $_SESSION['steamid'] ) ) {
             <?php if( ! empty( $_SESSION['steamid'] ) ):?>
                 <div class="rank-info">
                     <li class="rank-mini-li"><a href="javascript:void(0);" onclick="action_treeview()" >
-                        <div class="user-rank tooltip-right" data-tooltip="<?php echo $res_data_sidebar[0]['name_servers'];?>">
-                            <img src="<?php empty( $user_auth[0]['rank'] ) ? print '00' : print 'storage/cache/img/ranks/' . $res_data_sidebar[ 0 ]['ranks_pack'] . '/' . $user_auth[0]['rank']?>.png">
-                            <div class="rank-details">
-                                <?php echo $Modules->get_translate_phrase( $user_auth[0]['rank'], 'ranks_' . $res_data_sidebar[ 0 ]['ranks_pack'] )?>
+                            <div class="user-rank tooltip-right" data-tooltip="<?php echo $server_info[0]['name_servers'];?>">
+                                <img src="<?php empty( $user_auth[0]['rank'] ) ? print '00' : print 'storage/cache/img/ranks/' . $server_info[ 0 ]['ranks_pack'] . '/' . $user_auth[0]['rank']?>.png">
+                                <div class="rank-details">
+                                    <?php echo $Modules->get_translate_phrase( $user_auth[0]['rank'], 'ranks_' . $server_info[ 0 ]['ranks_pack'] )?>
+                                </div>
+                                <?php if ( $user_rank_count != 1 ):?>
+                                    <div class="icon-down">
+                                        <?php $General->get_icon( 'zmdi', 'chevron-down', null )?>
+                                    </div>
+                                <?php endif;?>
                             </div>
-                            <?php if ( $user_rank_count != 1 ):?>
-                            <div class="icon-down">
-                                <?php $General->get_icon( 'zmdi', 'chevron-down', null )?>
-                            </div>
-                            <?php endif;?>
-                        </div>
                         </a>
                         <ul class="treeview-menu">
                             <?php for ( $d = 1; $d < $user_rank_count; ++$d ):?>
                                 <li>
-                                    <div class="user-rank-more tooltip-right" data-tooltip="<?php echo $res_data_sidebar[ $d ]['name_servers']?>">
-                                        <img src="<?php empty( $user_auth[ $d ]['rank'] ) ? print '00' : print 'storage/cache/img/ranks/' . $res_data_sidebar[ $d ]['ranks_pack'] . '/' . $user_auth[ $d ]['rank']?>.png">
-                                        <div class="rank-details"><?php echo $Modules->get_translate_phrase( $user_auth[ $d ]['rank'], 'ranks_' . $res_data_sidebar[ $d ]['ranks_pack'] )?></div>
+                                    <div class="user-rank-more tooltip-right" data-tooltip="<?php echo $server_info[ $d ]['name_servers']?>">
+                                        <img src="<?php empty( $user_auth[ $d ]['rank'] ) ? print '00' : print 'storage/cache/img/ranks/' . $server_info[ $d ]['ranks_pack'] . '/' . $user_auth[ $d ]['rank']?>.png">
+                                        <div class="rank-details"><?php echo $Modules->get_translate_phrase( $user_auth[ $d ]['rank'], 'ranks_' . $server_info[ $d ]['ranks_pack'] )?></div>
                                     </div>
                                 </li>
                             <?php endfor;?>
@@ -97,37 +95,37 @@ if ( ! empty( $_SESSION['steamid'] ) ) {
                     </div>
                 </a>
             </li>
-<?php
-// Цикл -> Количество разделов sidebar
-for ( $d = 0, $c = sizeof( $Modules->arr_module_init['sidebar'] ); $d < $c; $d++ ):
+            <?php
+            // Цикл -> Количество разделов sidebar
+            for ( $d = 0, $c = sizeof( $Modules->arr_module_init['sidebar'] ); $d < $c; $d++ ):
 
-    // Цикл -> Счёт количества
-    for ( $_d = 0, $_c = sizeof( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'] ); $_d < $_c; $_d++ ):?>
-            <li class="tooltip-right" data-tooltip="<?php substr( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name'], 0, 1) == '_' ? print $Modules->get_translate_phrase($Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']) : print $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']?>">
-                <a href="<?php echo $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['href']?>" <?php get_section( 'page', 'home' ) == str_replace('?page=','',$Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['href']) ? print 'class="table-active"' : false?>
-                    <?php ( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['open_new_tab'] == true ) ? print 'target="_blank"' : false?>>
-                    <div class="sidebar-icon">
-                        <?php $General->get_icon( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['icon_group'], $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['icon'], $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['icon_category'] )?>
-                    </div>
-                    <div class="item-name">
-                        <?php substr( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name'], 0, 1) == '_' ? print $Modules->get_translate_phrase($Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']) : print $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']?>
-                    </div>
-                </a>
-            </li>
-<?php
-endfor;
-                  endfor;
-                  if( ! empty( $_SESSION['steamid32'] ) && $General->arr_general['admin'] == $_SESSION['steamid32'] ):?>
-            <li class="tooltip-right" data-tooltip="Панель администратора">
-                <a href="<?php echo get_url(2) . '?page=adminpanel'?>" <?php get_section( 'page', 'home' ) == 'adminpanel' && print 'class="table-active"'?>>
-                    <div class="sidebar-icon">
-                        <?php $General->get_icon( 'zmdi', 'coffee' )?>
-                    </div>
-                    <div class="item-name">
-                        Панель администратора
-                    </div>
-                </a>
-            </li>
+                // Цикл -> Счёт количества
+                for ( $_d = 0, $_c = sizeof( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'] ); $_d < $_c; $_d++ ):?>
+                    <li class="tooltip-right" data-tooltip="<?php substr( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name'], 0, 1) == '_' ? print $Modules->get_translate_phrase($Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']) : print $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']?>">
+                        <a href="<?php echo $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['href']?>" <?php get_section( 'page', 'home' ) == str_replace('?page=','',$Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['href']) ? print 'class="table-active"' : false?>
+                            <?php ( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['open_new_tab'] == true ) ? print 'target="_blank"' : false?>>
+                            <div class="sidebar-icon">
+                                <?php $General->get_icon( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['icon_group'], $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['icon'], $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['icon_category'] )?>
+                            </div>
+                            <div class="item-name">
+                                <?php substr( $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name'], 0, 1) == '_' ? print $Modules->get_translate_phrase($Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']) : print $Modules->array_modules[ $Modules->arr_module_init['sidebar'][ $d ] ]['sidebar'][ $_d ]['name']?>
+                            </div>
+                        </a>
+                    </li>
+                <?php
+                endfor;
+            endfor;
+            if( ! empty( $_SESSION['steamid32'] ) && $General->arr_general['admin'] == $_SESSION['steamid32'] ):?>
+                <li class="tooltip-right" data-tooltip="Панель администратора">
+                    <a href="<?php echo get_url(2) . '?page=adminpanel'?>" <?php get_section( 'page', 'home' ) == 'adminpanel' && print 'class="table-active"'?>>
+                        <div class="sidebar-icon">
+                            <?php $General->get_icon( 'zmdi', 'coffee' )?>
+                        </div>
+                        <div class="item-name">
+                            Панель администратора
+                        </div>
+                    </a>
+                </li>
             <?php endif;?>
         </ul>
     </section>
@@ -141,20 +139,20 @@ endfor;
                 <a href="#" title="Закрыть" class="modal-close badge"><?php $General->get_icon( 'zmdi', 'close' )?></a>
             </div>
             <form id="log_in" enctype="multipart/form-data" method="post">
-            <div class="login_form">
-            <div class="input-form">
-                <label>Логин</label>
-                <input type="text" value="" name="_login"/>
-            </div>
-            <div class="input-form">
-                <label>Пароль</label>
-                <input type="text" value="" name="_pass"/>
-            </div>
-                <?php if( $General->arr_general['steam_auth'] == 1 ):?>
-                    <a href="?auth=login">Войти по Steam</a>
-                <?php endif;?>
-                <input class="btn no_steam" name="log_in" nam type="submit" value="Войти">
-            </div>
+                <div class="login_form">
+                    <div class="input-form">
+                        <label>Логин</label>
+                        <input type="text" value="" name="_login"/>
+                    </div>
+                    <div class="input-form">
+                        <label>Пароль</label>
+                        <input type="text" value="" name="_pass"/>
+                    </div>
+                    <?php if( $General->arr_general['steam_auth'] == 1 ):?>
+                        <a href="?auth=login">Войти по Steam</a>
+                    <?php endif;?>
+                    <input class="btn no_steam" name="log_in" nam type="submit" value="Войти">
+                </div>
             </form>
         </div>
     </div>
