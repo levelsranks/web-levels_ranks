345ff9ff3213c6023ece21a3e146a30c15148a7e
Anastasia S <33180989+M0st1ce@users.noreply.github.com>
2019-10-27 15:42:42 +0300

    fix

--- modules/LK/module_page_lk_top/forward/data.php
+++ modules/LK/module_page_lk_top/forward/data.php
@@ -10,7 +10,7 @@
 
 $res = [];
 
-if( $Db->mysql_table_search('lk', 0, 0, 'lk_system') == true ):
+if( ! empty( $Db->db_data['lk'][0]['mod'] ) && $Db->db_data['lk'][0]['mod'] == 2 ):
     $res = $Db->queryAll( 'lk', 0, 0, "SELECT auth, `name`, all_money AS all_cash FROM lk_system WHERE all_money > 1 order by all_money desc");
 else:
     $res = $Db->queryAll( 'lk', 0, 0, "SELECT auth, `name`, all_cash FROM lk WHERE all_cash > 1 order by all_cash desc");
@@ -19,7 +19,7 @@ endif;
 $res == [] && header('Location: ' . $General->arr_general['site']);
 
 // Задаём заголовок страницы.
-$Modules->set_page_title( $General->arr_general['short_name'] . ' :: Топ донатеров :: ' . $Translate->get_translate_phrase('_Page') . ' ' . $page_num );
+$Modules->set_page_title( $General->arr_general['short_name'] . ' :: ' . $Translate->get_translate_module_phrase('module_page_lk_top', '_Top_donations') );
 
 // Задаём описание страницы.
-$Modules->set_page_description( $General->arr_general['short_name'] . ' :: Топ донатеров :: ' . $Translate->get_translate_phrase('_Page') . ' ' . $page_num );
+$Modules->set_page_description( $General->arr_general['short_name'] . ' :: ' .  $Translate->get_translate_module_phrase('module_page_lk_top', '_Top_donations') );
