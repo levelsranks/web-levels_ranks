857ba6bf97d157bf660df7b9159604d757eab954
Artur <artur.rusinov2013@ya.ru>
2020-06-01 00:41:15 +0400

    fix admin count

--- modules/SourceBans/module_block_main_sb_stats/forward/data.php
+++ modules/SourceBans/module_block_main_sb_stats/forward/data.php
@@ -15,18 +15,22 @@ $data['module_block_main_sb_stats'] = $Modules->get_module_cache('module_block_m
 
 // Проверяем актуальность кэша.
 if ( ( empty( $data['module_block_main_sb_stats'] ) ) || ( ! empty( $data['module_block_main_sb_stats']['time'] ) && time() > $data['module_block_main_sb_stats']['time'] ) ) {
-
-    // Затираем странные данные которые могут помешать созданию кэша.
-    $data['module_block_main_sb_stats']['count_admins'] = 0;
-    $data['module_block_main_sb_stats']['count_bans'] = 0;
-    $data['module_block_main_sb_stats']['count_comms'] = 0;
-    $data['module_block_main_sb_stats']['time'] = 0;
+    // Проверка на существование столбца expired
+    if ($Db->mysql_column_search('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][ 0 ]['DB_num'], $Db->db_data['SourceBans'][ 0 ]['Table'] . 'admins', 'expired')) {
+        $query_admins = "SELECT COUNT(1) FROM " . $Db->db_data['SourceBans'][ 0 ]['Table'] . "admins 
+                         WHERE expired = 0 or expired > '.time().' LIMIT 1";                            
+    } else {
+        $query_admins = "SELECT COUNT(1) FROM " . $Db->db_data['SourceBans'][ 0 ]['Table'] . "admins LIMIT 1"; 
+    }
 
     // Сохраняем текущее время и прибавляем к нему 1 час.
     $data['module_block_main_sb_stats']['time'] = time() + $Modules->array_modules['module_block_main_sb_stats']['setting']['cache_time'];
-    $data['module_block_main_sb_stats']['count_admins'] += $Db->queryNum('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][0]['DB_num'], 'SELECT COUNT(1) FROM ' . $Db->db_data['SourceBans'][ 0 ]['Table'] . 'admins WHERE expired = 0 or expired > '.time().' LIMIT 1' )[0]-1;
-    $data['module_block_main_sb_stats']['count_bans'] += $Db->queryNum('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][ 0 ]['DB_num'], 'SELECT COUNT(1) FROM ' . $Db->db_data['SourceBans'][ 0 ]['Table'] . 'bans LIMIT 1' )[0];
-    $data['module_block_main_sb_stats']['count_comms'] += $Db->queryNum('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][ 0 ]['DB_num'], 'SELECT COUNT(1) FROM ' . $Db->db_data['SourceBans'][ 0 ]['Table'] . 'comms LIMIT 1' )[0];
+    // Получаем кол-во админов
+    $data['module_block_main_sb_stats']['count_admins'] = $Db->queryNum('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][0]['DB_num'], $query_admins )[0]-1;
+    // Получаем кол-во банов
+    $data['module_block_main_sb_stats']['count_bans'] = $Db->queryNum('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][ 0 ]['DB_num'], 'SELECT COUNT(1) FROM ' . $Db->db_data['SourceBans'][ 0 ]['Table'] . 'bans LIMIT 1' )[0];
+    // Получаем кол-во мутов
+    $data['module_block_main_sb_stats']['count_comms'] = $Db->queryNum('SourceBans', $Db->db_data['SourceBans'][0]['USER_ID'], $Db->db_data['SourceBans'][ 0 ]['DB_num'], 'SELECT COUNT(1) FROM ' . $Db->db_data['SourceBans'][ 0 ]['Table'] . 'comms LIMIT 1' )[0];
     
     // Сохраняем новый кэш для данного модуля.
     $Modules->set_module_cache( 'module_block_main_sb_stats', $data['module_block_main_sb_stats'] );
