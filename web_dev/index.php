dd6b0cd796520c50f020b5001ca1e5f308d00af4
Nikita <62756604+FlamesONE@users.noreply.github.com>
2020-09-28 13:20:11 +0500

    Update index.php

--- web_dev/index.php
+++ web_dev/index.php
@@ -23,7 +23,7 @@ set_time_limit(4);
 define('IN_LR', true);
 
 // Версия LR WEB.
-define('VERSION', '0.2.133');
+define('VERSION', '0.2.134');
 
 // Основная директория вэб-приложения.
 define('APP', 'app/');
