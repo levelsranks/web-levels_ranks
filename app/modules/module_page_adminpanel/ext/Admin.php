fadd7dd0a03d94a62edc9bc8e65c7d8fd66eada1
ReDestroyDeR <daniil.schroder@gmail.com>
2022-04-22 23:04:35 +0300

    Revert "Admin Panel page reload fix"
    This reverts commit 5a35a299f307aa48616aac72d9c26e2cba068b79.

--- app/modules/module_page_adminpanel/ext/Admin.php
+++ app/modules/module_page_adminpanel/ext/Admin.php
@@ -34,9 +34,7 @@ class Admin
     //Позволяет обновить страницу без проблем с кавычками
     function ReloadPage()
     {
-        if (strlen($_SERVER['QUERY_STRING']) > 0) {
-            header("Location: ?".$_SERVER['QUERY_STRING']);
-        }
+        header("Location: ?".$_SERVER['QUERY_STRING']);
     }
 
     // Очистка кеша шаблонов
