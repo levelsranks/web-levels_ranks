eed3ddd11c63f90ffa76278522d700a08211806e
Nikita <62756604+FlamesONE@users.noreply.github.com>
2022-06-30 17:56:17 +0500

    lk balance fix

--- app/templates/default_dark/interface/head.php
+++ app/templates/default_dark/interface/head.php
@@ -1,6 +1,6 @@
 <body <?php $_SESSION['sidebar_open'] == false && print 'class="sidebar-collapse"'?>>
 <?php 
     $balance = $Modules->get_balance();
-    if( $balance )
+    if( $balance && $Modules->route != "lk" )
         $Modules->set_user_info_text($Translate->get_translate_module_phrase('module_page_lk_impulse','_Balance').': '.$Translate->get_translate_module_phrase('module_page_lk_impulse','_AmountCourse').' <b class="material-balance">'.$balance.'</b>');
 ?>
